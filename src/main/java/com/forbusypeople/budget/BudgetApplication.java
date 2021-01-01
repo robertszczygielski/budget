37cec86cd7081bfadf4c6120826a0ef500510a47
Robert Szczygielski <robert.szczygielski@wp.pl>
2021-01-01 17:14:43 +0100

    First commit

---
+++ src/main/java/com/forbusypeople/budget/BudgetApplication.java
@@ -0,0 +1,18 @@
+package com.forbusypeople.budget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication(exclude = {
+		DataSourceAutoConfiguration.class,
+		SecurityAutoConfiguration.class
+})
+public class BudgetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetApplication.class, args);
+	}
+
+}
