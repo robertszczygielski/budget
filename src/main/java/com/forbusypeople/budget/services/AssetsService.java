3be53f8615ec71415a1193606989abb7e965003e
Robert Szczygielski <robert.szczygielski@wp.pl>
2021-02-11 21:01:30 +0100

    (012) Java, Spring Boot, Logowanie zdarzeń

--- src/main/java/com/forbusypeople/budget/services/AssetsService.java
+++ src/main/java/com/forbusypeople/budget/services/AssetsService.java
@@ -4,6 +4,8 @@ import com.forbusypeople.budget.mappers.AssetsMapper;
 import com.forbusypeople.budget.repositories.AssetsRepository;
 import com.forbusypeople.budget.services.dtos.AssetDto;
 import com.forbusypeople.budget.validators.AssetValidator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,6 +13,8 @@ import java.util.stream.Collectors;
 
 @Service
 public class AssetsService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(AssetsService.class.getName());
 
     private AssetsRepository assetsRepository;
     private AssetsMapper assetsMapper;
@@ -23,28 +27,38 @@ public class AssetsService {
     }
 
     public List<AssetDto> getAllAssets() {
+        LOGGER.debug("Get all assets");
         return assetsRepository.findAll().stream()
                 .map(entity -> assetsMapper.fromEntityToDto(entity))
                 .collect(Collectors.toList());
     }
 
     public void setAsset(AssetDto dto) {
+        LOGGER.info("Set Asset");
+        LOGGER.debug("AssetDto: " + dto);
         assetValidator.validate(dto);
         var entity = assetsMapper.fromDtoToEntity(dto);
 
         assetsRepository.save(entity);
+        LOGGER.info("Asset Saved");
     }
 
     public void deleteAsset(AssetDto dto) {
+        LOGGER.info("Delete asset");
+        LOGGER.debug("AssetDto: " + dto);
         var entity = assetsMapper.fromDtoToEntity(dto);
         assetsRepository.delete(entity);
+        LOGGER.info("Asset deleted");
     }
 
     public void updateAsset(AssetDto dto) {
+        LOGGER.info("Update asset");
+        LOGGER.debug("AssetDto: " + dto);
         var entity = assetsRepository.findById(dto.getId());
         entity.ifPresent(e -> {
             e.setAmount(dto.getAmount());
             assetsRepository.saveAndFlush(e);
         });
+        LOGGER.info("Asset updated");
     }
 }
