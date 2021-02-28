3e5d8a080bdad5fff707a46d92aa0a44798ff16c
Robert Szczygielski <robert.szczygielski@wp.pl>
2021-02-28 11:42:47 +0100

    (018) Spring Security - podstawowe, proste logowanie do aplikacji

---
+++ src/main/java/com/forbusypeople/budget/services/UserDetailsServiceImpl.java
@@ -0,0 +1,18 @@
+package com.forbusypeople.budget.services;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        return new User("admin", "admin", Collections.emptyList());
+    }
+}
