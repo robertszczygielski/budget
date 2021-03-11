bf4cc24bdeceba8e171a5edda02eacefdaa23790
Robert Szczygielski <robert.szczygielski@wp.pl>
2021-03-11 21:16:32 +0100

    (020) Java - wykorzystanie tokena JWT do logowania do aplikacji

--- src/main/java/com/forbusypeople/budget/services/JWTService.java
+++ src/main/java/com/forbusypeople/budget/services/JWTService.java
@@ -1,5 +1,6 @@
 package com.forbusypeople.budget.services;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -12,8 +13,32 @@ import java.util.Map;
 @Service
 public class JWTService {
 
-    private final int milisecondsInDay = 86400000;
-    private final String secret = "mySecret";
+    private final int MILISECONDS_IN_DAY = 86400000;
+    private final String SECRET = "mySecret";
+
+    public String extractUserName(String token) {
+        var claims = extractClaims(token);
+        return claims.getSubject();
+    }
+
+    public Date extractExpirationDate(String token) {
+        var claims = extractClaims(token);
+        return claims.getExpiration();
+    }
+
+    public Boolean validateToken(String token, UserDetails userDetails) {
+        var userName = extractUserName(token);
+        return userName.equals(userDetails.getUsername()) && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpirationDate(token).before(new Date());
+    }
+
+    private Claims extractClaims(String token) {
+        return Jwts.parser().setSigningKey(SECRET).parseClaimsJws(token).getBody();
+    }
+
 
     public String generateJWTToken(UserDetails userDetails) {
         Map<String, Object> claims = new HashMap<>();
@@ -22,9 +47,9 @@ public class JWTService {
         return Jwts.builder()
                 .setSubject(userDetails.getUsername())
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + milisecondsInDay))
-                .setClaims(claims)
-                .signWith(SignatureAlgorithm.HS256, secret)
+                .setExpiration(new Date(System.currentTimeMillis() + MILISECONDS_IN_DAY))
+                .addClaims(claims)
+                .signWith(SignatureAlgorithm.HS256, SECRET)
                 .compact();
     }
 
