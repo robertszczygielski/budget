41793b5639bc54f75b363fd7f0eecd1a2449e3a6
Robert Szczygielski <robert.szczygielski@wp.pl>
2021-03-11 21:16:32 +0100

    Using jwt token to authentication admin user

--- src/main/java/com/forbusypeople/budget/services/AuthenticationService.java
+++ src/main/java/com/forbusypeople/budget/services/AuthenticationService.java
@@ -2,6 +2,8 @@ package com.forbusypeople.budget.services;
 
 import com.forbusypeople.budget.services.dtos.AuthenticationJwtToken;
 import com.forbusypeople.budget.services.dtos.AuthenticationUserDto;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
 
@@ -10,13 +12,21 @@ public class AuthenticationService {
 
     private final UserDetailsService userDetailsService;
     private final JWTService jwtService;
+    private final AuthenticationManager authenticationManager;
 
-    public AuthenticationService(UserDetailsServiceImpl userDetailsService, JWTService jwtService) {
+    public AuthenticationService(UserDetailsServiceImpl userDetailsService,
+                                 JWTService jwtService,
+                                 AuthenticationManager authenticationManager) {
         this.userDetailsService = userDetailsService;
         this.jwtService = jwtService;
+        this.authenticationManager = authenticationManager;
     }
 
     public AuthenticationJwtToken createAuthenticationToken(AuthenticationUserDto authenticationUserDto) {
+
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
+                authenticationUserDto.getUsername(), authenticationUserDto.getPassword()
+        ));
 
         var userDetails = userDetailsService.loadUserByUsername(authenticationUserDto.getUsername());
         var jwtToken = jwtService.generateJWTToken(userDetails);
